e0cbf245c1333f25cf44070a0780280ef4a38abb
Geovane Pereira <geeo.sp@gmail.com>
2020-09-20 21:20:19 -0300

    Fix update page script

--- update_gh_page.bat
+++ update_gh_page.bat
@@ -1,12 +1,11 @@
 ::build and commit the docs page
 RMDIR /Q/S docs
 cd resume
-flutter update
-flutter pub get
-flutter build web --release
-cd ..
-git add docs/*
-git commit -m "Update Page Build"
-git push origin main
+::flutter upgrade
+"flutter pub get & flutter build web --release "
+cd .. 
+git add docs/* 
+git commit -m "Update Page Build" 
+git push origin main 
 PAUSE
 
